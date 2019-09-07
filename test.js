f776489cbf6c8ee767cbaf58c98040d7d637c17f
KaMeHb-UA <marlock@etlgr.com>
2019-09-07 11:29:01 -0700

    updated tests

--- test.js
+++ test.js
@@ -1,2 +1,33 @@
-const a = require('.');
-console.log(a.sync())
+const { sync, async, registerListener } = require('.');
+
+async function test(f){
+    const start = new Date;
+    const result = await f();
+    const end = new Date;
+    console.log(`
+*****************************
+function ${f.name | 'anonymous'}:
+execution took ${end - start}ms
+with result`, result, `
+*****************************`);
+}
+
+function testCallback(f){
+    let lastTime = new Date;
+    f((...args) => {
+        const now = new Date;
+        console.log(`
+*****************************
+callback for ${f.name | 'anonymous'}:
+last time executed ${now - lastTime}ms ago
+executed with args:`, ...args, `
+*****************************`);
+        lastTime = now;
+    })
+}
+
+(async () => {
+    await test(sync);
+    await test(async);
+    testCallback(registerListener);
+})()
